7c9cc7ca7ddaeffb357901837a069ae3ec566f1f
fancy45daddy <124528204+fancy45daddy@users.noreply.github.com>
2023-07-18 00:55:21 -0700

    Update myself.py

--- myself.py
+++ myself.py
@@ -14,7 +14,7 @@ async def main():
     site = aiohttp.web.TCPSite(runner, '0.0.0.0', 8000)
     await site.start()
     async with aiohttp.ClientSession(headers={'user-agent':fake_useragent.UserAgent().chrome}) as client:
-        async with client.get('https://myself-bbs.com/forum.php?mod=viewthread&tid=44639&extra=page%3D2%26filter%3Dtypeid%26typeid%3D539%26typeid%3D539') as episode:
+        async with client.get('https://myself-bbs.com/forum.php?mod=viewthread&tid=49677&extra=page%3D1%26filter%3Dtypeid%26typeid%3D539%26typeid%3D539') as episode:
             html = bs4.BeautifulSoup(await episode.text(), 'lxml')
             title = zhconv.convert(re.split('[／【]', html.find('title').string)[0].replace(' ', ''), 'zh-cn')
             for _ in itertools.islice(html.find('ul', attrs={'class', 'main_list'}).find_all('li', recursive=False), 0, None):
