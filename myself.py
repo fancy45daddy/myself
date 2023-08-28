6e1c765c089be50608aed57e93d18841c5a055b8
fancy45daddy <124528204+fancy45daddy@users.noreply.github.com>
2023-08-28 09:07:35 -0700

    Update myself.py

--- myself.py
+++ myself.py
@@ -17,7 +17,7 @@ async def main():
         async with client.get('https://myself-bbs.com/forum.php?mod=viewthread&tid=45128&highlight=dr.stone') as episode:
             html = bs4.BeautifulSoup(await episode.text(), 'lxml')
             title = zhconv.convert(re.split('[／【]', html.find('title').string)[0].replace(' ', ''), 'zh-cn')
-            for _ in itertools.islice(html.find('ul', attrs={'class', 'main_list'}).find_all('li', recursive=False), 0, None):
+            for _ in itertools.islice(html.find('ul', attrs={'class', 'main_list'}).find_all('li', recursive=False), 19, None):
                 async with client.ws_connect('wss://v.myself-bbs.com/ws') as ws:
                     href = urllib.parse.urlparse(_.find('a', attrs={'data-href':True}).get('data-href')).path
                     if 'play/' in href:
