62e67a0bf8157e9adf773ff1fe4a81b8f872a5eb
fancy45daddy <124528204+fancy45daddy@users.noreply.github.com>
2023-09-01 09:58:17 -0700

    Update myself.py

--- myself.py
+++ myself.py
@@ -14,10 +14,10 @@ async def main():
     site = aiohttp.web.TCPSite(runner, '0.0.0.0', 8000)
     await site.start()
     async with aiohttp.ClientSession(headers={'user-agent':fake_useragent.UserAgent().chrome}) as client:
-        async with client.get('https://myself-bbs.com/forum.php?mod=viewthread&tid=45128&highlight=dr.stone') as episode:
+        async with client.get('https://myself-bbs.com/thread-46597-1-1.html') as episode:
             html = bs4.BeautifulSoup(await episode.text(), 'lxml')
             title = zhconv.convert(re.split('[／【]', html.find('title').string)[0].replace(' ', ''), 'zh-cn')
-            for _ in itertools.islice(html.find('ul', attrs={'class', 'main_list'}).find_all('li', recursive=False), 19, None):
+            for _ in itertools.islice(html.find('ul', attrs={'class', 'main_list'}).find_all('li', recursive=False), 0, None):
                 async with client.ws_connect('wss://v.myself-bbs.com/ws') as ws:
                     href = urllib.parse.urlparse(_.find('a', attrs={'data-href':True}).get('data-href')).path
                     if 'play/' in href:
